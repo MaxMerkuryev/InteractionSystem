ac74c0776a90f970ede034efb51a7f9451010953
AAAAAAAAA <maxmerkury13@gmail.com>
2024-04-02 19:21:59 +0700

    interaction prompt ui

---
+++ Assets/App/Features/InteractionSystem/Code/Ui/UiInteractionPrompt.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class UiInteractionPrompt : MonoBehaviour {
+	[SerializeField] private CanvasGroup _prompt;
+
+	private InteractionHandler _interactionHandler;
+
+	[Inject]
+	public void Construct(InteractionHandler interactionHandler) {
+		_interactionHandler = interactionHandler;
+		_interactionHandler.Focused += Show;
+		_interactionHandler.Unfocused += Hide;
+		_prompt.alpha = 0;
+	}	
+
+	private void OnDestroy() {
+		_interactionHandler.Focused -= Show;
+		_interactionHandler.Unfocused -= Hide;			
+	}
+
+	private void Show() {
+		_prompt.alpha = 1;
+	}
+
+	private void Hide() {
+		_prompt.alpha = 0;
+	}
+}
---
+++ Assets/App/Installers/SceneInstaller.cs
@@ -0,0 +1,10 @@
+﻿using Zenject;
+using UnityEngine;
+
+public class SceneInstaller : MonoInstaller {
+	[SerializeField] private InteractionHandler _interactionHandler;	
+
+	public override void InstallBindings() {
+		Container.BindInstance(_interactionHandler).AsSingle();
+	}
+}
