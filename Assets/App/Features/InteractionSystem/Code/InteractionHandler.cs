b86211840490d39a8123b205240cd96c4c8eac83
AAAAAAAAA <maxmerkury13@gmail.com>
2024-04-02 18:35:29 +0700

    remove selection outline

--- Assets/App/Features/InteractionSystem/Code/InteractionHandler.cs
+++ Assets/App/Features/InteractionSystem/Code/InteractionHandler.cs
@@ -4,16 +4,9 @@ public class InteractionHandler : MonoBehaviour {
 	[SerializeField] private Transform _source;
 	[SerializeField] private float _reach;
 
-	private ISelectable _focusedSelectable;
-
 	private void Update() {
 		if (!GetRaycast(out RaycastHit hit)) {
-			ClearFocus();
 			return;
-		}
-
-		if (GetHitComponent(hit, out ISelectable selectable)) {
-			Focus(selectable);
 		}
 
 		if (GetHitComponent(hit, out IInteractable interactable)) {
@@ -26,24 +19,7 @@ public class InteractionHandler : MonoBehaviour {
 		if (!Input.GetMouseButtonDown(0)) return;
 		
 		interactable.Interact();
-	}
-
-	private void Focus(ISelectable selectable) {
-		if (selectable == null) return;
-		if (selectable == _focusedSelectable) return;
-
-		ClearFocus();
-
-		_focusedSelectable = selectable;
-		_focusedSelectable.Focus();
-	}
-
-	private void ClearFocus() {
-		if (_focusedSelectable == null) return;
-
-		_focusedSelectable.Unfocus();
-		_focusedSelectable = null;
-	}
+	}	
 
 	private bool GetRaycast(out RaycastHit hit) {
 		Ray ray = new Ray(_source.position, _source.forward);
